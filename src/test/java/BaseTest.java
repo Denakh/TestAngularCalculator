4cfdf66e682c01edaca0d2bda660a93cadf59b19
Denakh <denakhokh@gmail.com>
2018-10-15 23:50:40 +0200

    Lesson 11 HW

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -9,18 +9,15 @@ public class BaseTest {
     protected WebDriver driver;
     protected AngularCalculatorPage angularCalculatorPage;
 
-    //@BeforeMethod(alwaysRun = true)
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver_win32\\chromedriver.exe");
-        //ChromeDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.way2automation.com/angularjs-protractor/calc/");
         angularCalculatorPage = new AngularCalculatorPage(driver);
     }
 
-    //@AfterMethod(alwaysRun = true)
     @AfterClass(alwaysRun = true)
     public void tearDown() throws Exception {
         driver.close();
